0f416f781c21bcba04d621822e85358a520fdb0c
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-10-19 14:37:05 +0100

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -1,2 +1,10 @@
 # Display the titles of the latest 10 posts in r/ShowerThoughts
 # https://www.reddit.com/r/showerthoughts/new/.json?limit=10
+
+import json
+import requests
+
+def jprint(obj):
+    """Create a formatted string of the Python JSON object"""
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
