9a6add747b142663f01f535a6070edb26d4d5903
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-10-19 14:49:44 +0100

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -8,3 +8,19 @@ def jprint(obj):
     """Create a formatted string of the Python JSON object"""
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
+
+url = "https://www.reddit.com/r/ShowerThoughts/new/.json"
+parameters = {"limit": 10}
+response = requests.get(url, params=parameters)
+
+if response.status_code != 200:
+    print("Page is currently unavailable, try again later.")
+else:
+    #jprint(response.json())
+    post_titles = []
+    for post in response.json():
+        title = post["data"]["children"][0]["data"]["title"]
+        post_titles.append(title)
+    for i, post in enumerate(post_titles, start=1):
+        print(f"{i}: {post}")
+        
